1bcccd3ee3a2743be683272816a5558f0be6f267
Luis Eguia <jlewicho@gmail.com>
2018-04-26 00:34:44 -0500

    Metodo para obtener todos los libros organizados por categorias.

--- application/controllers/Libros.php
+++ application/controllers/Libros.php
@@ -3,20 +3,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH . '/libraries/REST_Controller.php';
 
 class Libros extends REST_Controller {
-
     public function __construct($config = 'rest') {
-        header('Access-Control-Allow-Origin: *');
-        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-        $method = $_SERVER['REQUEST_METHOD'];
-        if($method == "OPTIONS") {
-          die();
-        }
-        parent::__construct();
+      header('Access-Control-Allow-Origin: *');
+      header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+      header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+      $method = $_SERVER['REQUEST_METHOD'];
+      if($method == "OPTIONS") {
+        die();
+      }
+      parent::__construct();
 
-        $this->load->database();
-        $this->load->helper('url');
-		    $this->load->model("Modelo");
+      $this->load->database();
+      $this->load->helper('url');
+	    $this->load->model("Modelo");
     }
 
 		public function libros_get(){
@@ -24,11 +23,11 @@ class Libros extends REST_Controller {
 			$this->response($arr);
 		}
 
-		public function libro_materia_get(){
-			$categorias = $this->input->get("materia");
+		public function libro_categoria($categoria){
+			// $categorias = $this->input->get("materia");
 
-			$arr = $this->Modelo->query("SELECT libros.* FROM categorias JOIN libros_has_categorias ON (libros_has_categorias.categoria_id = categorias.id_categoria) JOIN libros ON (libros_has_categorias.libro_id = libros.id_libro) WHERE categorias.nombre = ?", $categorias);
-			$this->response($arr);
+			$arr = $this->Modelo->query("SELECT libros.* FROM categorias JOIN libros_has_categorias ON (libros_has_categorias.categoria_id = categorias.id_categoria) JOIN libros ON (libros_has_categorias.libro_id = libros.id_libro) WHERE categorias.nombre = ?", $categoria);
+			return $arr;
 		}
 
 		public function libro_get(){
@@ -40,5 +39,18 @@ class Libros extends REST_Controller {
 			} else {
 				$this->response("No se ha encontrado libro.");
 			}
+		}
+
+		public function categorias_get(){
+			$arr_categorias = [];
+			$categorias = $this->Modelo->query("SELECT * FROM categorias");
+
+			if (count($categorias) > 0) {
+			  foreach ($categorias as $categoria) {
+					$arr_categorias[$categoria->nombre] = $this->libro_categoria($categoria->nombre);
+			  }
+			}
+
+			$this->response($arr_categorias);
 		}
 }
